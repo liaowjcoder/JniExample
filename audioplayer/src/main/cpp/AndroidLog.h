2f4f0d42dd3f5103a1394ddec4f544187de9937b
liaoweijian <lwj_zeal@163.com>
2018-11-25 22:50:25 +0800

    解码音频数据

--- audioplayer/src/main/cpp/AndroidLog.h
+++ audioplayer/src/main/cpp/AndroidLog.h
@@ -1,10 +1,4 @@
-
-#ifndef MYMUSIC_ANDROIDLOG_H
-#define MYMUSIC_ANDROIDLOG_H
-
-
 #include "android/log.h"
-
 
 #define LOGD(FORMAT, ...) __android_log_print(ANDROID_LOG_DEBUG,"liaoweijian",FORMAT,##__VA_ARGS__);
 #define LOGE(FORMAT, ...) __android_log_print(ANDROID_LOG_ERROR,"liaoweijian",FORMAT,##__VA_ARGS__);
